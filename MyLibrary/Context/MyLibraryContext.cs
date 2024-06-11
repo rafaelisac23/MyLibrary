f868d09a6336c0dbc56ce1d996bfb815bd9cbe98
rafael isac <rafael.darrigo@gmail.com>
2024-06-11 14:37:43 -0300

    Base DBContext EF

---
+++ MyLibrary/Context/MyLibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLibrary.Models; //para usar o DbContext precisa do using
+
+namespace MyLibrary.Context
+{
+    public class MyLibraryContext : DbContext
+    {
+        public MyLibraryContext(DbContextOptions<MyLibraryContext>options ) :base (options)
+        {
+            
+        }
+        public DbSet<Book>? Books { get; set; }
+        public DbSet<Loan>? Loans { get; set; }
+        public DbSet<Publisher>? Publishers{ get; set; }
+        public DbSet<Student>? Students { get; set; }
+    }
+}
