685fd1afbc8ba482c5672e1cd3647452ea395ae2
rafael isac <rafael.darrigo@gmail.com>
2024-06-11 15:00:36 -0300

    Final DBContext EF

--- MyLibrary/Program.cs
+++ MyLibrary/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MyLibrary.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//nunca esqueça que pra poder fazer conexão com o banco voce tem que adcionar esse codigo abaixo
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MyLibraryContext>(options => options.UseMySql(mySqlConnection,
+    ServerVersion.AutoDetect(mySqlConnection)));
+//fim
 
 var app = builder.Build();
 
